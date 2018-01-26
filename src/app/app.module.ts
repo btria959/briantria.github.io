28427e7938dc1b6a3f464b00f8e9346d362e4c4c
Brian Tria <btria959@gmail.com>
2018-01-26 22:36:44 +0400

    add navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { NavigationComponent } from './navigation/navigation.component';
 import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectsComponent,
-    NavigationComponent
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
