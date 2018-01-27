2bcb0d96fca74e345f5012d06cd92dcae93772d6
Brian Tria <btria959@gmail.com>
2018-01-27 10:39:50 +0400

    create PROJECTS constant to simulate db fetch

--- src/app/_object-classes/project.ts
+++ src/app/_object-classes/project.ts
@@ -3,3 +3,53 @@ export class Project {
     role: string;
     links: {};
 }
+
+export const PROJECTS: Project[] = [
+{
+    name: 'MLB Line Drive',
+    role: "Lead Software Developer",
+    links: [
+    {
+        platform: "Android", 
+        url: "https://play.google.com/store/apps/details?id=com.mlb.SlamCity15&hl=en"
+    },
+    {
+        platform: "iOS",
+        url: "https://itunes.apple.com/us/app/mlb-com-line-drive/id992156829?mt=8"
+    }
+    ]
+},
+{
+    name: "Yu Gi Oh! Duel Generation",
+    role: "Frontend Software Developer",
+    links: [
+    {
+        platform: "Android", 
+        url: "https://play.google.com/store/apps/details?id=com.konami.ygodgtest&hl=en"
+    },
+    {
+        platform: "iOS",
+        url: "https://itunes.apple.com/us/app/yu-gi-oh-duel-generation/id883078087?mt=8"
+    }
+    ]
+},
+{
+    name: "Tinted",
+    role: "Frontend Software Developer",
+    links: [
+    {
+        platform: "Android", 
+        url: "https://play.google.com/store/apps/details?id=com.lbstudios.tinted&hl=en"
+    }
+    ]
+},
+{
+    name: "Throw with Joe",
+    role: "Full Stack Software Developer",
+    links: [
+    {
+        platform: "iOS (Review)", 
+        url: "https://www.tapscape.com/throw-with-joe-iphone-game-review/"
+    }
+    ]
+}];
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Project } from '../_object-classes/project'
+import { Project, PROJECTS } from '../_object-classes/project'
 
 @Component({
   selector: 'app-projects',
@@ -8,56 +8,7 @@ import { Project } from '../_object-classes/project'
 })
 export class ProjectsComponent implements OnInit {
   // TODO: fetch from db (firebase? google sheets?)
-  projects: Project[] = [
-    {
-      name: 'MLB Line Drive',
-      role: "Lead Software Developer",
-      links: [
-        {
-          platform: "Android", 
-          url: "https://play.google.com/store/apps/details?id=com.mlb.SlamCity15&hl=en"
-        },
-        {
-          platform: "iOS",
-          url: "https://itunes.apple.com/us/app/mlb-com-line-drive/id992156829?mt=8"
-        }
-      ]
-    },
-    {
-      name: "Yu Gi Oh! Duel Generation",
-      role: "Frontend Software Developer",
-      links: [
-        {
-          platform: "Android", 
-          url: "https://play.google.com/store/apps/details?id=com.konami.ygodgtest&hl=en"
-        },
-        {
-          platform: "iOS",
-          url: "https://itunes.apple.com/us/app/yu-gi-oh-duel-generation/id883078087?mt=8"
-        }
-      ]
-    },
-    {
-      name: "Tinted",
-      role: "Frontend Software Developer",
-      links: [
-        {
-          platform: "Android", 
-          url: "https://play.google.com/store/apps/details?id=com.lbstudios.tinted&hl=en"
-        }
-      ]
-    },
-    {
-      name: "Throw with Joe",
-      role: "Full Stack Software Developer",
-      links: [
-        {
-          platform: "iOS (Review)", 
-          url: "https://www.tapscape.com/throw-with-joe-iphone-game-review/"
-        }
-      ]
-    }
-  ];
+  projects: Project[] = PROJECTS;
 
   constructor() { }
 
